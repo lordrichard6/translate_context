81e9b151d3e85a2271356e989e84a53452fb52fb
lordrichard6 <paulolopesreizinho@gmail.com>
2021-02-16 18:42:25 +0100

    connecting the field and the button to the store

--- src/components/Field.js
+++ src/components/Field.js
@@ -5,7 +5,7 @@ class Field extends React.Component {
   static contextType = LanguageContext;
 
   render() {
-    const text = this.context === "english" ? "Name" : "Naan";
+    const text = this.context.language === "english" ? "Name" : "Naan";
 
     return (
       <div className="ui field">
